db7f98d71cef84e49260db9fbcec09c7764ab447
Ryan Johnson <rjohnson18@ua.edu>
2024-01-17 15:32:26 -0700

    changed location of AWS log in

--- tethysapp/map_layout_tutorial/public/js/roset_aws.js
+++ tethysapp/map_layout_tutorial/public/js/roset_aws.js
@@ -1,51 +1,32 @@
-let checkCsrfSafe = function (method) {
-  // these HTTP methods do not require CSRF protection
-  return /^(GET|HEAD|OPTIONS|TRACE)$/.test(method);
-}
-
-let getCookie = function (name) {
-  let cookie;
-  let cookies;
-  let cookieValue = null;
-  let i;
-
-  if (document.cookie && document.cookie !== "") {
-        cookies = document.cookie.split(";");
-        for (i = 0; i < cookies.length; i += 1) {
-              cookie = $.trim(cookies[i]);
-              // Does this cookie string begin with the name we want?
-              if (cookie.substring(0, name.length + 1) === name + "=") {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break
-              }
-        }
+// The following function are copying from 
+// https://docs.djangoproject.com/en/dev/ref/csrf/#ajax
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
   }
   return cookieValue;
-};
-function fetchData() {
-
-
-  fetch(".", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      "X-CSRFToken": getCookie("csrftoken")
-    },
-    body: {
-      method: "update-dates",
-      other: 1,
-      another: true
-    },
-  })
-  .then((response) => console.log(response))
-  .then((data) => {
-    // Handle the response data
-    console.log(data);
-    // Perform actions with the received data
-  })
-  .catch((error) => {
-    // Handle errors
-    console.log(error);
-  });
 }
-fetchData()
+let csrftoken = getCookie('csrftoken');
+
+
+
+fetch(".", {
+   method: 'POST',
+   headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "X-CSRFToken": csrftoken 
+   },
+   body:
+      'method=test_rest_api&other=1'
+}).then((response) => response.json()).then((data) => {
+// Do something magical with your data :)
+});s
