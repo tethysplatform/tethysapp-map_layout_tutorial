63abf50c8b1cee95bd0663e4724a26409d669f11
romer8 <gio.busrom@gmail.com>
2024-02-01 15:58:08 -0700

    a couple of useful additions for the production environment

--- tethysapp/community_streamflow_evaluation_system/controllers.py
+++ tethysapp/community_streamflow_evaluation_system/controllers.py
@@ -33,9 +33,12 @@ from django.http import HttpResponse
 from .utils import combine_jsons, reach_json
 
 #Set Global Variables
-
-ACCESS_KEY_ID = app.get_custom_setting('Access_key_ID')
-ACCESS_KEY_SECRET = app.get_custom_setting('Secret_access_key')
+try:
+    ACCESS_KEY_ID = app.get_custom_setting('Access_key_ID')
+    ACCESS_KEY_SECRET = app.get_custom_setting('Secret_access_key')
+except Exception:
+    ACCESS_KEY_ID = ''
+    ACCESS_KEY_SECRET = ''
 
 #AWS Data Connectivity
 #start session
