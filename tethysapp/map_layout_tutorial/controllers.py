229bac4921d5bcfe0206e63cdd878af8d0cfb44e
jakeymac <jakeymacaroni@gmail.com>
2024-08-16 17:51:59 -0600

    updates for tethys-4.3

--- tethysapp/map_layout_tutorial/controllers.py
+++ tethysapp/map_layout_tutorial/controllers.py
@@ -3,15 +3,15 @@ from pathlib import Path
 import pandas as pd
 from tethys_sdk.layouts import MapLayout
 from tethys_sdk.routing import controller
-from .app import MapLayoutTutorial as app
+from .app import App
 
 
 MODEL_OUTPUT_FOLDER_NAME = 'sample_nextgen_data'
 
 @controller(name="home", app_workspace=True)
 class MapLayoutTutorialMap(MapLayout):
-    app = app
-    base_template = 'map_layout_tutorial/base.html'
+    app = App
+    base_template = f'{App.package}/base.html'
     map_title = 'Map Layout Tutorial'
     map_subtitle = 'NOAA-OWP NextGen Model Outputs'
     default_map_extent = [-87.83371926334216, 33.73443611122197, -86.20833410475134, 34.456557011634175]
@@ -71,7 +71,7 @@ class MapLayoutTutorialMap(MapLayout):
         ]
 
         return layer_groups
-    
+
     @classmethod
     def get_vector_style_map(cls):
         return {
@@ -99,7 +99,7 @@ class MapLayoutTutorialMap(MapLayout):
         }
 
     def get_plot_for_layer_feature(self, request, layer_name, feature_id, layer_data, feature_props, app_workspace,
-                                   *args, **kwargs):
+                                *args, **kwargs):
         """
         Retrieves plot data for given feature on given layer.
 
